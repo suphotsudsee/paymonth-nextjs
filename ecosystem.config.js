5eb2abb3353cf5ad6f57e4c91fd94dba987527d6
suphot <suphotsudsee@gmail.com>
2025-12-03 13:58:57 +0700

    tt

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   apps: [
     {
-      name: 'carbooking-nextjs',
+      name: 'paymonth-nextjs',
       script: 'npm',
       args: 'start',
-      cwd: '/home/phoubon-suphot/htdocs/test-carbooking.phoubon.in.th',
+      cwd: '/home/phoubon-suphot/htdocs/test-paymonth.phoubon.in.th',
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
       env: {
         NODE_ENV: 'production',
-        PORT: 9100,
+        PORT: 9102,
       },
       env_production: {
         NODE_ENV: 'production',
-        PORT: 9100,
+        PORT: 9102,
       },
       env_development: {
         NODE_ENV: 'development',
-        PORT: 9100,
+        PORT: 9102,
       },
       time: true,
       log_date_format: 'YYYY-MM-DD HH:mm Z',
@@ -33,8 +33,8 @@ module.exports = {
       user: 'phoubon-suphot',
       host: 'localhost',
       ref: 'origin/main',
-      repo: 'https://github.com/suphotsudsee/carbooking-nextjs.git',
-      path: '/home/phoubon-suphot/htdocs/test-carbooking.phoubon.in.th',
+      repo: 'https://github.com/suphotsudsee/paymonth-nextjs.git',
+      path: '/home/phoubon-suphot/htdocs/test-paymonth.phoubon.in.th',
       'pre-deploy-local': '',
       'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
